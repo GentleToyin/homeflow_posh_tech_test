745454c648de7e76167e618aebd4b2f25375bbfc
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2022-10-18 16:37:18 +0100

    Submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
-import Header from './Header';
-import PropertyCard from './PropertyCard';
+import { useState, useEffect } from "react";
+import Header from "./Header";
+import PropertyCard from "./PropertyCard";
 
 function App() {
   const [properties, setProperties] = useState();
-
   // use this state to keep track of the user's saved/bookmarked properties
   const [savedProperties, setSavedProperties] = useState([]);
-
+  // this state keeps track of the searched query
+  const [searchedQuery, setSearchedQuery] = useState("");
   useEffect(() => {
     const fetchPropertyData = async () => {
-      const response = await fetch('/property-data.json');
+      const response = await fetch("/property-data.json");
       const json = await response.json();
 
       setProperties(json.result.properties.elements);
@@ -18,13 +18,41 @@ function App() {
 
     fetchPropertyData();
   }, []);
+  const handleSearch = (e) => {
+    setSearchedQuery(e.toLowerCase());
+  };
 
+  const handleSavedProperties = (savedProperty) => {
+    if (savedProperties.includes(savedProperty)) {
+      let newArray = savedProperties.filter((property) => {
+        return property.property_id !== savedProperty.property_id;
+      });
+      setSavedProperties(newArray);
+    } else {
+      setSavedProperties([...savedProperties, savedProperty]);
+    }
+  };
+
+  console.log(properties);
   return (
     <div className="container mx-auto my-5">
-      <Header />
-
-      <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {!!properties && properties.map((property) => <PropertyCard key={property.property_id} property={property} />)}
+      <div className="container fixed z-10  bg-white -mt-20 shadow-sm">
+        <Header handleSearch={handleSearch} />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mt-20 pt-14 md:pt-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {!!properties &&
+          properties
+            .filter((property) =>
+              property.short_description.toLowerCase().includes(searchedQuery)
+            )
+            .map((property) => (
+              <PropertyCard
+                key={property.property_id}
+                property={property}
+                handleSavedProperties={handleSavedProperties}
+                savedProperties={savedProperties}
+              />
+            ))}
       </div>
     </div>
   );
--- src/PropertyCard.jsx
+++ src/PropertyCard.jsx
@@ -1,16 +1,46 @@
-import { FaBookmark } from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import { FaBookmark } from "react-icons/fa";
+import image from "./assets/house_img.jpg";
 
-function PropertyCard({ property }) {
+function PropertyCard({ property, savedProperties, handleSavedProperties }) {
+  const [isSaved, setIsSaved] = useState(false);
+  useEffect(() => {
+    handleSavedIndicator();
+  });
+
+  const handleSavedIndicator = () => {
+    setIsSaved(false);
+    savedProperties.map((savedPty) => {
+      return savedPty.property_id === property.property_id && setIsSaved(true);
+    });
+  };
   return (
-    <div className="border-2 bg-gray-50">
+    <div className="border-2 rounded-lg bg-gray-50 shadow-sm hover:shadow-md">
       <div className="relative">
-        <img src={`https://mr0.homeflow.co.uk/${property.photos[0]}`} alt={property.display_address} />
+        <img
+          src={
+            property.photos.length < 1
+              ? image
+              : `https://mr0.homeflow.co.uk/${property.photos[0]}`
+          }
+          alt={property.display_address}
+          className="rounded-lg"
+        />
 
-        <button className="absolute top-0 right-2" title="Click to bookmark this property">
-          <FaBookmark className="text-yellow-400" size="40" />
+        <button
+          className="absolute top-0 right-2"
+          title="Click to bookmark this property"
+          onClick={() => handleSavedProperties(property)}
+        >
+          <FaBookmark
+            className={isSaved ? "text-red-400" : "text-yellow-400"}
+            size="40"
+          />
         </button>
 
-        <p className="absolute bottom-0 right-0 px-2 py-1 border-t border-l bg-gray-50">{property.price}</p>
+        <p className="absolute bottom-0 right-0 px-2 py-1 border-t border-l bg-gray-50">
+          {property.price}
+        </p>
       </div>
 
       <div className="px-3 py-2">
@@ -18,6 +48,6 @@ function PropertyCard({ property }) {
       </div>
     </div>
   );
-};
+}
 
 export default PropertyCard;
